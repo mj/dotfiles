2b19e5f1ae06c9028fdfbfc1f9dec24ecace3fa5
Martin Jansen <martin@divbyzero.net>
2017-04-18 15:06:47 +0200

    Add deployment rules for Vim configuration

--- deploy-all.sh
+++ deploy-all.sh
@@ -2,9 +2,10 @@
 
 PREFIX=$HOME
 
-for file in .bash_profile .bashrc .gitconfig .inputrc .screenrc .wgetrc .zshrc joerc
+for file in .bash_profile .bashrc .gitconfig .inputrc .screenrc .wgetrc .zshrc joerc .vimrc
 do
 	cp -va $file $PREFIX/$file
 done
 
 rsync -av --delete .zsh/ $PREFIX/.zsh/
+rsync -av --delete .vim/ $PREFIX/.vim/
